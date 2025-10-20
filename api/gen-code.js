9c3cd2f4d348c03ced8350b5c21fb843cffeb530
Paulo de Castro <134304646+paulocastropn@users.noreply.github.com>
2025-10-20 10:50:54 -0400

    Fix formatting of code generation string

--- api/gen-code.js
+++ api/gen-code.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
 
   // Generate code
   const rand = () => Math.random().toString(36).substring(2, 8).toUpperCase();
-  const code = `BICA15-${rand()}`;
+  const code = `BICA15-${rand()}`; 
   const now = new Date();
   const expires = new Date(now.getTime() + 14*24*60*60*1000);
 
